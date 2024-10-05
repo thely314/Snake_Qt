754f46b525a397c863894508c8472db58ae726e3
lone <1715849169@qq.com>
2024-10-05 19:39:25 +0800

    untracking build files

--- Snake/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Snake_FILE C:/msys64/home/dyl/Codes/qt/Snake/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/Snake.exe)
-set(__QT_DEPLOY_TARGET_Snake_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Snake_RUNTIME_DLLS C:/thely/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/thely/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/thely/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
